9e03622877d9d4bb999511221dc7350897c97385
Shumaila550 <codecanvas78@gmail.com>
2026-03-29 19:23:33 +0500

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,29 @@
+function renderSkills() {
+  const grid = document.getElementById("skills-grid");
 
+  skills.forEach(skill => {
+    const div = document.createElement("div");
+    div.innerHTML = `${skill.name} - ${skill.pct}%`;
+    grid.appendChild(div);
+  });
+}
+
+function renderProjects() {
+  const grid = document.getElementById("projects-grid");
+
+  projects.forEach(p => {
+    const div = document.createElement("div");
+    div.textContent = p.title;
+    grid.appendChild(div);
+  });
+}
+
+function renderServices() {
+  const grid = document.getElementById("services-grid");
+
+  services.forEach(s => {
+    const div = document.createElement("div");
+    div.textContent = s.title;
+    grid.appendChild(div);
+  });
+}
